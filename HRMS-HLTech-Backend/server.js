8cfef3b53f5f4992a54082a5bd7ccc223deed3e2
shanni kant <shannimura8683@gmail.com>
2026-06-07 19:46:49 +0530

    cors update

--- HRMS-HLTech-Backend/server.js
+++ HRMS-HLTech-Backend/server.js
@@ -51,13 +51,23 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://hrms-project-lovat.vercel.app'
+    ],
     methods: ['GET', 'POST'],
+    credentials: true
   },
 });
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://hrms-project-lovat.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({
